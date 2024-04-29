5221ccb868243f2ab44d73d55c0923d19a0b0945
aadityasharma8707 <168389783+aadityasharma8707@users.noreply.github.com>
2024-04-29 12:18:58 +0530

    Add files via upload

---
+++ ConnectFourTutorial/ConnectFourTutorial/BoardCell.swift
@@ -0,0 +1,14 @@
+//
+//  BoardCell.swift
+//  ConnectFourTutorial
+//
+//  Created by Students on 4/22/24.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var image: UIImageView!
+}
